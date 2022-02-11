82b22cf08f89aa997527d8b80e1ace34a04a0e0f
Mariia Svitelska <maria.svitelska@ukr.net>
2022-02-11 23:20:11 +0200

    Added task2 and task3 of HW2

---
+++ homework2/task2.py
@@ -0,0 +1,4 @@
+my_first_name = 'Mariia'
+my_last_name = ' Svitelska'
+my_full_name = my_first_name + my_last_name
+print('Welcome my dear', my_full_name, '!')
---
+++ homework2/task3.py
@@ -0,0 +1,11 @@
+a = 7
+b = 9
+
+# Python calculator that can make different operations for e.x. +, -, /, *, **, %, //
+print('Addition a + b =', a + b, '\n',
+      'Subtraction a - b = ', a - b, '\n', 'Subtraction b - a = ', b - a, '\n',
+      'Division a / b = ', a / b, '\n', 'Division b / a =', b / a, '\n',
+      'Multiplication a * b =', a * b, '\n',
+      'Exponent Power a ** b = ', a ** b, '\n', 'Exponent Power b ** a = ', b ** a, '\n',
+      'Modulus a % b =', a % b, '\n', 'Modulus b % a =', b % a, '\n',
+      'Floor division a // b =', a // b, '\n', 'Floor division b // a = ', b // a)

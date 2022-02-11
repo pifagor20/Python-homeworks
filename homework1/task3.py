d97bdb5f1db8be76fd3b56b1342e380c2916db1d
Mariia Svitelska <maria.svitelska@ukr.net>
2022-02-11 16:40:52 +0200

    Task 3 changes

--- homework1/task3.py
+++ homework1/task3.py
@@ -1,12 +1,19 @@
-print('#########', end='\n')
-print('#', '#', sep='\t\t', end='\n')
-print('#', '#', sep='\t\t', end='\n')
-print('#', '#', sep='\t\t', end='\n')
-print('#########', end='\n \n')
+# print('#########', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
+# print('#########', end='\n \n')
+#
+# print('#', '#', sep='\t\t', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
+# print('#########', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
+# print('#', '#', sep='\t\t', end='\n')
 
-print('#', '#', sep='\t\t', end='\n')
-print('#', '#', sep='\t\t', end='\n')
-print('#########', end='\n')
-print('#', '#', sep='\t\t', end='\n')
-print('#', '#', sep='\t\t', end='\n')
+x = '#'
+y = x*10
+# Print statement to display O letter
+print(y, '\n' + x, '\t\t', x, '\n' + x, '\t\t', x, '\n' + x, '\t\t', x, '\n' + y, '\n')
 
+# Print statement to display H letter
+print(x, '\t\t', x, '\n' + x, '\t\t', x, '\n' + y, '\n' + x, '\t\t', x, '\n' + x, '\t\t', x, '\n')
